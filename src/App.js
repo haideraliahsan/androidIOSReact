f2cac2488afbe3d09392289317d4a318da2ff5fc
Muhammad Hassam <m.hassam@goldevelopers.com>
2019-11-12 15:44:46 +0500

    run on ios

--- src/App.js
+++ src/App.js
@@ -1,8 +1,9 @@
 import React from 'react';
-import { View, Text, TouchableOpacity } from 'react-native';
+import { View, Text, TouchableOpacity,Platform ,NativeModules} from 'react-native';
 import ShowBox from '../ShowBoxNativeView'
 
 class App extends React.Component {
+
     render() {
         console.disableYellowBox=true;
         return (
@@ -11,7 +12,21 @@ class App extends React.Component {
                     Prees Here to see Native UI
                 </Text>
                 <View style={{ justifyContent: 'center', alignContent: 'center', alignSelf: 'center', marginTop: 20 }}>
+                    {Platform.OS=='android'
+                    ?
                     <ShowBox exampleProp="Hassam" style={{ width: 120, height: 50 }} />
+                    :
+                    <TouchableOpacity onPress={()=>{
+                            // Add object of native modules
+                    }}>
+                        <View style={{backgroundColor:'yellow'}}>
+                        <Text style={{fontSize=20}}>
+                            Run AR
+                        </Text>
+                        </View>
+                       
+                    </TouchableOpacity>
+                    }
                 </View>
             </View>
         )
